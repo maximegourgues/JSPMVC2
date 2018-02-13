3fac9a9ccee52b9d6a7eac6fd94dd1215fd324c9
Rémi Bastide <Remi.Bastide@irit.fr>
2018-02-13 14:52:54 +0100

    Minor changes

--- src/main/java/servlet/exampleController.java
+++ src/main/java/servlet/exampleController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlet;
 
 import java.io.IOException;
@@ -12,10 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author rbastide
- */
+
 @WebServlet(name = "exampleController", urlPatterns = {"/exampleController"})
 public class exampleController extends HttpServlet {
 
