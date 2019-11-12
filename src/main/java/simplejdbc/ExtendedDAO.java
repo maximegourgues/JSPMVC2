dfa71d619ea5078b4acc07e268cec9acc2eb8e0f
pedago <pedago@pcalb-mm0608.univ-jfc.fr>
2019-11-12 14:55:30 +0100

    Discount Controller

--- src/main/java/simplejdbc/ExtendedDAO.java
+++ src/main/java/simplejdbc/ExtendedDAO.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import javax.sql.DataSource;
 
 public class ExtendedDAO extends DAO {
@@ -37,5 +39,21 @@ public class ExtendedDAO extends DAO {
 			throw new DAOException(e.getMessage());
 		}
 		return result;
-	}	
+	}
+        public Map<String,Float> existingDiscountCode() throws DAOException {
+		Map<String,Float> result = new HashMap<>();
+		String sql = "SELECT * FROM DISCOUNT_CODE";
+		try (	Connection connection = myDataSource.getConnection(); 
+			Statement stmt = connection.createStatement(); 
+			ResultSet rs = stmt.executeQuery(sql)) {
+			while (rs.next()) {
+				String code = rs.getString("DISCOUNT_CODE");
+                                float rate = rs.getFloat("RATE");
+				result.put(code,rate);
+			}
+		} catch (SQLException e) {
+			throw new DAOException(e.getMessage());
+		}
+		return result;
+	}
 }
